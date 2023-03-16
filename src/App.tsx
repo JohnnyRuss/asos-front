61400ecec58730c44844c578e8fb8c76e2e59a5c
JohnnyRuss <georgiaspontoeli123@gmail.com>
2023-03-16 05:12:18 +0400

    refacture naviigations json

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-// import useAppStore from "./store/app";
-
 import Navigation from "./components/Navigation/Navigation";
 import HomePage from "./Pages/HomePage";
 import ProductsPage from "./Pages/ProductsPage";
@@ -9,15 +7,6 @@ import ProductPage from "./Pages/ProductPage";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-  // const products = useAppStore((state) => state.products);
-
-  // const productTypes = Array.from(
-  //   new Set(
-  //     products.map((product) => product.productType).flatMap((type) => type)
-  //   )
-  // );
-
-  // console.log(productTypes);
   return (
     <>
       <Navigation />
